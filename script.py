7b6404a1df864bf1167d0926599d86db24e70506
roryc89 <roryc89@googlemail.com>
2019-05-14 14:46:43 -0400

    adds some plots

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 orders = pd.read_csv("data/Orders.csv")
-
-orders.info()
-
-orders.Profit.head()
 
 orders.Profit = pd.to_numeric(
     orders.Profit.map(lambda x: re.sub("[\$\.,]", "", x))
@@ -14,3 +12,28 @@ orders.Profit = pd.to_numeric(
 orders.Sales = pd.to_numeric(
     orders.Sales.map(lambda x: re.sub("[\$\.,]", "", x))
 )
+
+orders["date"] = pd.to_datetime(orders["Order.Date"])
+
+orders["month"] = orders.date.map(lambda d: d.month)
+orders["year"] = orders.date.map(lambda d: d.year)
+
+plt.figure(figsize=(32, 16))
+
+orders.groupby(["year", "month"]).agg("count")["Row.ID"].barplot(
+    kind="bar", subplots=True, orient="h"
+)
+
+
+counts = orders.groupby(["year", "month"]).agg("count")["Row.ID"]
+
+counts.map(plt.hist)
+
+g = sns.FacetGrid(counts)
+
+g = g.map(plt.hist, "year")
+# .unstack(1).plot(
+#     kind="bar", subplots=True
+# )
+
+plt.show()
